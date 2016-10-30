3048d5897c5d8fe1fc3013f7b264ad3b42009423
Iggy <igoririanto@rocketmail.com>
2016-10-29 17:09:18 -0700

    refactor play/pause

--- app/scripts/services/SongPlayer.js
+++ app/scripts/services/SongPlayer.js
@@ -1,10 +1,8 @@
 (function(){
-  function SongPlayer() {
+  function SongPlayer(Fixtures) {
     var SongPlayer = {};
-    /**
-    * @desc initialize currentSong var to be null
-    */
-    var currentSong = null; //sets up currentSong var
+    var currentAlbum = Fixtures.getAlbum();
+
     /**
     * @desc Buzz object audio file
     * @type {Object}
@@ -19,15 +17,15 @@
     var setSong = function(song) { //private method
       if (currentBuzzObject) {
         currentBuzzObject.stop();
-        currentSong.playing = null;
+        SongPlayer.currentSong.playing = null;
       }
 
       currentBuzzObject = new buzz.sound(song.audioUrl, {
         formats: ['mp3'],
         preload: true
       });
+      SongPlayer.currentSong = song;
 
-      currentSong = song;
     };
 
     /**
@@ -40,12 +38,22 @@
       song.playing = true;
     };
 
+    var getSongIndex = function(song) {
+      return currentAlbum.songs.indexOf(song);
+    };
     //public method
+
+    /**
+    * @desc initialize currentSong var to be null
+    */
+    SongPlayer.currentSong = null; //sets up currentSong public attribute
+
     SongPlayer.play = function(song) { //object. Question: Can I do this.play = function(song) instead of SongPlayer.play?
-        if (currentSong !== song) { //if currently playing song is not the song that is chosen
+        song = song || SongPlayer.currentSong;
+        if (SongPlayer.currentSong !== song) { //if currently playing song is not the song that is chosen
             setSong(song);
             playSong(song);
-        } else if (currentSong === song) { //else if we are hovering on song #2 and clicks song#2 (2 possibility: is song#2 playing? is song#2 paused?)
+        } else if (SongPlayer.currentSong === song) { //else if we are hovering on song #2 and clicks song#2 (2 possibility: is song#2 playing? is song#2 paused?)
           if (currentBuzzObject.isPaused()) { //if it is paused, then play it
             playSong(song);
           }
@@ -54,9 +62,24 @@
 
     //public method
     SongPlayer.pause = function(song) {
+      song = song || SongPlayer.currentSong;
       currentBuzzObject.pause();
       song.playing = false;
     }
+
+    SongPlayer.previous = function(){
+      var currentSongIndex = getSongIndex(SongPlayer.currentSong);
+      currentSongIndex--;
+
+      if (currentSongIndex < 0) {
+        currentBuzzObject.stop();
+        SongPlayer.currentSong.playing = null;
+      } else {
+        var song = currentAlbum.songs[currentSongIndex];
+        setSong(song);
+        playSong(song);
+      }
+    };
 
     return SongPlayer;
   }
