8d59577f6013cb04a3b871c8cd6d7473c76c203c
Iggy <igoririanto@rocketmail.com>
2016-11-01 21:17:16 -0700

    refactor timer using buzz toTimer() method

--- app/scripts/filters/timecode.js
+++ app/scripts/filters/timecode.js
@@ -1,7 +1,7 @@
 (function() {
   function timecode() {
       return function(seconds) { //filter functions must return another function accepting >= 1 argument
-        var seconds = Number.parseFloat(seconds);
+      /*  var seconds = Number.parseFloat(seconds);
         if (Number.isNaN(seconds)) {
           return '-:--';
         }
@@ -17,11 +17,18 @@
         }
 
         output += remainingSeconds;
-        return output;
+        return output;*/
+
+        /*
+        * @desc refactor timer using buzz.toTimer() method
+        * in 00:00 format
+        */
+        var timer = buzz.toTimer(seconds);
+        return timer;
       };
   }
 
   angular
       .module('blocJams')
-      .filter('timecodez', timecode);
+      .filter('timecode', timecode);
 })();
